a94b14fd9ae4a750862194f22c565bad400c7faf
WilliamLindskog <williamlindskog@protonmail.com>
2023-06-13 15:55:58 +0200

    First commit

---
+++ utils/data_processing.py
@@ -0,0 +1,26 @@
+import json
+import pandas as pd
+
+# Get encoder
+# from sklearn.preprocessing import LabelEncoder
+
+df = pd.read_csv('./data/fever.csv')
+
+# Drop fuel_date
+df = df.drop(['fuel_date'], axis=1)
+
+# Encode all object columns
+# Store cat codes and its corresponding categories
+cat_codes = {}
+for col in df.columns:
+    if df[col].dtypes == 'object':
+        df[col] = df[col].astype('category')
+        cat_codes[col] = dict(enumerate(df[col].cat.categories))
+        df[col] = df[col].cat.codes
+
+# Save cat codes
+with open('./data/cat_codes.json', 'w') as f: 
+    json.dump(cat_codes, f)
+
+# Save encoded data
+df.to_csv('./data/fever_encoded.csv', index=False)
